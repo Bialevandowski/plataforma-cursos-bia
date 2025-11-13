c056055e70e7c090571e75a6592e4dbe532cf7e1
Bialevandowski <bianca.levandowski@edu.unipar.br>
2025-11-12 21:00:36 -0300

    Adicionando página Home

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
 import Home from "./pages/Home";
-import Cursos from "./pages/Cursos";
-import Login from "./pages/Login";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 function App() {
-  return (
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cursos" element={<Cursos />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-        <Footer />
-      </Router>
-  );
+    return <Home />;
 }
 
 export default App;
